5b14934c488e732667bb8ceeb6166129c995b134
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-03-17 12:02:21 +0200

    Calendar added.

--- src/main/java/projectdefence/interceptor/RequestProcessingTimeInterceptor.java
+++ src/main/java/projectdefence/interceptor/RequestProcessingTimeInterceptor.java
@@ -14,16 +14,16 @@ import javax.servlet.http.HttpServletResponse;
 public class RequestProcessingTimeInterceptor implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory
             .getLogger(RequestProcessingTimeInterceptor.class);
-//
-//    @Override
-//    public boolean preHandle(HttpServletRequest request,
-//                             HttpServletResponse response, Object handler) {
-//        long startTime = System.currentTimeMillis();
-//        logger.info("Request URL::" + request.getRequestURL().toString()
-//                + ":: Start Time=" + System.currentTimeMillis());
-//        request.setAttribute("startTime", startTime);
-//        return true;
-//    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) {
+        long startTime = System.currentTimeMillis();
+        logger.info("Request URL::" + request.getRequestURL().toString()
+                + ":: Start Time=" + System.currentTimeMillis());
+        request.setAttribute("startTime", startTime);
+        return true;
+    }
 
     @Override
     public void postHandle(HttpServletRequest request,
@@ -32,14 +32,14 @@ public class RequestProcessingTimeInterceptor implements HandlerInterceptor {
         logger.info("Request URL::" + request.getRequestURL().toString()
                 + " Sent to Handler :: Current Time=" + System.currentTimeMillis());
     }
-//
-//    @Override
-//    public void afterCompletion(HttpServletRequest request,
-//                                HttpServletResponse response, Object handler, Exception ex) {
-//        long startTime = (Long) request.getAttribute("startTime");
-//        logger.info("Request URL::" + request.getRequestURL().toString()
-//                + ":: End Time=" + System.currentTimeMillis());
-//        logger.info("Request URL::" + request.getRequestURL().toString()
-//                + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
-//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex) {
+        long startTime = (Long) request.getAttribute("startTime");
+        logger.info("Request URL::" + request.getRequestURL().toString()
+                + ":: End Time=" + System.currentTimeMillis());
+        logger.info("Request URL::" + request.getRequestURL().toString()
+                + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+    }
 }
