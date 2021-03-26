388266e9a176f66510856159f040b964cab4815f
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-03-26 18:31:07 +0200

    Tests problem.

--- src/test/java/projectdefence/HomeControllerTest.java
+++ src/test/java/projectdefence/HomeControllerTest.java
@@ -1,11 +1,8 @@
 package projectdefence;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
