fc46a75f8fd249b174eafa4b330f224721ba8682
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-06-18 12:06:38 +0300

    Motorious - Center for kinesiotherapy.

--- src/main/java/projectdefence/models/binding/UserRegisterBindingModel.java
+++ src/main/java/projectdefence/models/binding/UserRegisterBindingModel.java
@@ -84,6 +84,9 @@ public class UserRegisterBindingModel {
     @Email(message = INVALID_EMAIL)
     @NotBlank(message = EMPTY_FIELD)
     @Length(max = 150, message = INVALID_EMAIL)
+    // More advance check
+    //  @Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+    //    message = "Enter a valid email address!")
     public String getEmail() {
         return email;
     }
@@ -95,6 +98,9 @@ public class UserRegisterBindingModel {
 
     @Length(min = 6, max = 50, message = LENGTH_BETWEEN_6_50_CHARACTERS)
     @NotBlank(message = EMPTY_FIELD)
+    // More advance check
+    // @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d\\w\\W]{8,}$",
+    //            message = "Password must contain minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character!")
     public String getPassword() {
         return password;
     }
