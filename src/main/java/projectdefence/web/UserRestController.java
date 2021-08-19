705183a6a1a919d77abdf9856839f3508394afba
Stefan Markov <62558905+StefanFMarkov@users.noreply.github.com>
2021-08-19 22:18:12 +0300

    Motorious - Center for kinesiotherapy.

---
+++ src/main/java/projectdefence/web/UserRestController.java
@@ -0,0 +1,36 @@
+package projectdefence.web;
+
+import org.apache.http.protocol.HTTP;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import projectdefence.models.viewModels.UserWrapInfoViewModel;
+import projectdefence.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+    private final ModelMapper modelMapper;
+    private final UserService userService;
+
+    public UserRestController(ModelMapper modelMapper, UserService userService) {
+        this.modelMapper = modelMapper;
+        this.userService = userService;
+    }
+
+    @GetMapping("/all-users")
+    @PreAuthorize("hasRole('ROLE_ROOT')")
+    @ResponseStatus(HttpStatus.FOUND)
+    @ResponseBody
+    public List<UserWrapInfoViewModel> allUsers(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "limit", defaultValue = "2") int limit) {
+
+        return userService.getUsersPage(page, limit);
+    }
+
+}
